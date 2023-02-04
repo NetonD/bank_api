75809ddfedc0d6f5e617d98eaa4c49dd74079eb9
Almir Neto <almir.as.neto03@gmail.com>
2023-02-04 13:57:52 -0300

    test: creating behaviour expected test for account creation

---
+++ test/bank_api_web/controllers/account_controller_test.exs
@@ -0,0 +1,29 @@
+defmodule BankApiWeb.AccountControllerTest do
+  use BankAPIWeb.ConnCase
+
+  @create_attrs %{
+    initial_balance: 42_00
+  }
+
+  @invalid_attrs %{
+    initial_balance: nil
+  }
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "create account" do
+    test "render account when have valid attrs", %{conn: conn} do
+      conn = post(conn, Routes.account_path(conn, :create, @create_attrs))
+
+      assert %{"uuid" => _uuid, "account_balance" => 42_00} = json_response(201, conn)["data"]
+    end
+
+    test "render error when data is invalid", %{conn: conn} do
+      conn = post(conn, Routes.account_path(conn, :create, @invalid_attrs))
+
+      assert json_response(201, conn)["errors"] != %{}
+    end
+  end
+end
